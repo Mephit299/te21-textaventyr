447cad772da6a39b6d02a8219c7c40af461b6a4c
Mephit299 <levihogdal@gmail.com>
2024-01-16 11:13:49 +0100

    404, comit message not found

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,22 @@ router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome', part: story.parts[0] })
 })
 
+router.post('/username', function(req, res){
+  req.session.username = req.body.username
+  console.log(req.session.username)
+  res.redirect('/story/0')
+})
+
 router.get('/story/:id', function (req, res) {
-  const part = story.parts.find((part) => part.id === parseInt(req.params.id))
+  let part = story.parts.find((part) => part.id === parseInt(req.params.id))
   if (!part) {
     res.status(404).render('404.njk', { title: '404' })
     return
   }
-  res.render('part.njk', { title: part.name, part: part })
+
+  const name = part.name.replace('[PLAYER]', req.session.username)
+  part = { ...part, name: name}
+  res.render('part.njk', { title: name, part: part })
 })
 
 module.exports = router
